8d6f5d6411f86513e9a8cb896e3b0033e9d3e65d
Gabriel Aranda <per.gabrielaranda@gmail.com>
2024-01-28 21:04:48 -0300

    feat: added migration to create schema

--- migrations/20240112100000_create_schema.js
+++ migrations/20240112100000_create_schema.js
@@ -3,14 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-  const exists = await knex.schema.hasSchema('orders')
-  if (!exists) {
-    return knex.schema.createSchema('orders')
-  }
+  return knex.schema.createSchemaIfNotExists('orders')
 }
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {}
+exports.down = function (knex) {
+  return knex.schema.dropSchemaIfExists('orders')
+}
